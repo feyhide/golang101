c1074f280c3f7186e636ec86037b1d5a623eae07
feyhide <fahadsheikhessential@gmail.com>
2025-03-23 08:16:27 +0500

    added gracefully shutdown

--- 28_webapp/cmd/webapp/main.go
+++ 28_webapp/cmd/webapp/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/feyhide/golang101/28_webapp/internal/config"
 )
@@ -26,9 +31,36 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Printf("[+] Server Started on %s \n\n", cfg.HTTPServer.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Warning: Failed to start server")
+	slog.Info("Server Started", slog.String("address", cfg.HTTPServer.Addr))
+
+	//----WITH GRACEFULLY SHUTDOWN----
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal("Warning: Failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down The server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
 	}
+
+	slog.Info("Server shutdown gracefully")
+
+	//----WITHOUT GRACEFULLY SHUTDOWN----
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Fatal("Warning: Failed to start server")
+	// }
 }
